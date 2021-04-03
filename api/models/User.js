ffe2489bf589949b9ed7e572cd751afafad8a34f
James Wheadon <james.wheadon@yahoo.com>
2021-04-03 12:15:20 +0100

    Add User create function

--- api/models/User.js
+++ api/models/User.js
@@ -1,11 +1,21 @@
 const db = require ('../dbConfig/init');
 
-class User {
+module.exports = class User {
     contructor(data){
         this.id = data.id; 
         this.username = data.username;
         this.password = data.password; 
     }
-
-  
+    
+    static async create(userData){
+        return new Promise (async (res, rej) => {
+            try {
+                const {username, passwordHashed} = userData;
+                await db.query('INSERT INTO users (username, password) VALUES ($1, $2);', [username, passwordHashed]);
+                res('User created');
+            } catch (err) {
+                rej('User could not be created');
+            }
+        })
+    }
 } 
