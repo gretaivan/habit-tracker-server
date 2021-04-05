48f76be8298ee8b772abd8303408ba69cd21c583
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-05 14:30:48 +0100

    add habit post route and begin create habit functionality

---
+++ api/controllers/Habit.js
@@ -0,0 +1,11 @@
+//create Habit Route 
+// habit_name and frequency will be the only paramaters chosen by the user, others will be set automatically to 0
+
+router.post('/', async (req, res) => {
+    try {
+        const habit = await Habit.create(req.body.habit_name, req.body.frequency)
+        res.status(201).json(habit)
+    } catch(err) {
+        res.status(404).json({err})
+    }
+})
--- api/models/Habit.js
+++ api/models/Habit.js
@@ -5,14 +5,31 @@ class Habit {
         this.id = data.id;
         this.habit_name = data.habit_name; 
         this.frequency = data.frequency;
-        this.completed = data.completed;
-        this.no_times_completed = data.no_times_completed;  
-        this.last_comp = data.last_comp;
+        this.completed = data.completed; //what is this for?
+        this.no_times_completed = data.no_times_completed;  //do we need this?
+        this.last_comp = data.last_comp; // do we need this? can we not just get it from comp_dates table 
         this.comp_dates_id = data.comp_dates_id;
+        // this.user_id = data.user_id - do I need a user id to connect a specific havit to
+    }
+
+
+    static create(habit_name, frequency){
+        return new Promise (async (resolve, reject) => {
+            try {
+                let habitData = await db.query(`INSERT INTO habits (habit_name, frequency) VALUES ($1, $2) RETURNING *;`, [ habit_name, frequency ]);
+                // I want to get back habit Data with an id, habit_name, frequency, comp_dates_id, the rest need to start at 0 
+                let newHabit = new Habit(habitData.rows[0]);
+                resolve (newHabit);
+            } catch (err) {
+                reject('Error creating habit');
+            }
+        });
     }
 
   
 } 
+
+
 
 // add habit 
 
