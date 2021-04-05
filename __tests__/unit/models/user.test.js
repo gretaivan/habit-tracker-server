36e50a62079f8234953776beb39daffb5f4fb0ce
gretaivan <greta.ivan91@gmail.com>
2021-04-05 17:55:34 +0100

    adds create user with error handling and test

--- __tests__/unit/models/user.test.js
+++ __tests__/unit/models/user.test.js
@@ -1,20 +1,45 @@
-const { TestScheduler } = require('@jest/core');
+// const { TestScheduler } = require('@jest/core');
 const User = require('../../../models/User');
+// const { unsubscribe } = require('../../../server');
+const db = require('../../../dbConfig/init');
+const e = require('express');
 
+let userData = {username: 'test', password: 'pass'};
 
 describe('User model', () => {
+    beforeEach(() => jest.clearAllMocks());
+
+    afterAll(() => jest.resetAllMocks());
+
     describe('create', ()=> {
-        it('takes in data object and creates a User instance', () => {
-            expect.toBeInstanceOf(User); 
+        
+        // it('takes in data object and creates a User instance', () => {
+        //     expect.toBeInstanceOf(User); 
+        // });
+       
+        it('creates user and generates id', async () => {
+            
+            jest.spyOn(db, 'query').mockResolvedValueOnce({rows: [ { ...userData, id: 1}] });
+            const result = await User.create(userData);
+            expect(result).toHaveProperty('id'); 
         });
-        it('creates user and generates id', () => {
-            expect.toHaveProperty('id'); 
+
+        
+        it('creates user and returns a user instance', async () => {
+        jest.spyOn(db, 'query').mockResolvedValueOnce({rows: [ { ...userData, id: 1}] });
+        let result = await User.create(userData);
+        expect(result).toBeInstanceOf(User);
         });
+
         test.each([
-            [{username: 'test', password: 'pass'}, User],
-            [{ username: 'test'}, Error]
-          ])('create with test case %#', (testData, expected) => {
-            expect(testData).toBeInstanceOf(expected);
-          });
+            [{username: null, password: 'pass'}],
+            [{username: 'test'}],
+            [{password: 'test3'}]
+        ])('rejects empty values in the user test case %#', async (testCase) => {
+            
+            // let result = await User.create(testCase);
+            await expect(User.create(testCase)).rejects.toEqual('ERROR: user could not be created');
+        });
+
     })
 });
--- models/User.js
+++ models/User.js
@@ -1,15 +1,41 @@
 const db = require ('../dbConfig/init');
 
 class User {
-    contructor(data){
+    constructor(data){
         this.id = data.id; 
         this.username = data.username;
         this.password = data.password; 
     }
-
-    //TODO: create
-
     //TODO: get all users
+    static get all(){
+        return new Promise(async(res, rej) => {
+            try{
+                let result = await db.query(`SELECT * from users;`)
+                const users = result.rows.map(user => new User(user));
+                res(users); 
+            } catch(err){
+                rej('ERROR: user could not get the user record');
+            }
+        });
+    }
+    // TODO: create
+    static create(data){
 
+        return new Promise(async (res, rej) => {
+            try{
+                let userResult = await db.query(`INSERT INTO users (username, password)
+                                            VALUES($1, $2) RETURNING *;`, [data.username, data.password]);                  
+                let user = new User(userResult.rows[0]);
+                console.log(user);
+                res(user); 
+            } catch(err) {
+                rej('ERROR: user could not be created');
+            }
+        });
+    };
+
+    
   
 } 
+
+module.exports = User;
