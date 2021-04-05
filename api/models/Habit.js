ec23779c6b0a3c85c03c470aecf9b52adac61c4a
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-05 13:26:27 +0100

    add model for habit

---
+++ api/models/Habit.js
@@ -0,0 +1,20 @@
+
+
+class Habit {
+    contructor(data){
+        this.id = data.id;
+        this.habit_name = data.habit_name; 
+        this.frequency = data.frequency;
+        this.completed = data.completed;
+        this.no_times_completed = data.no_times_completed;  
+        this.last_comp = data.last_comp;
+        this.comp_dates_id = data.comp_dates_id;
+    }
+
+  
+} 
+
+// add habit 
+
+
+
