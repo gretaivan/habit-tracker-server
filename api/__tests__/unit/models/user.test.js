75939fb621f5a1629982e0dbf2b817cd92a3a339
gretaivan <greta.ivan91@gmail.com>
2021-04-05 19:15:26 +0100

    adds get all users with test. Adds error handling for user model, tests edge cases.cleans the user test suite

--- api/__tests__/unit/models/user.test.js
+++ api/__tests__/unit/models/user.test.js
@@ -1,45 +1,52 @@
-// const { TestScheduler } = require('@jest/core');
 const User = require('../../../models/User');
-// const { unsubscribe } = require('../../../server');
-const db = require('../../../dbConfig/init');
-const e = require('express');
+
 
 let userData = {username: 'test', password: 'pass'};
 
 describe('User model', () => {
     beforeEach(() => jest.clearAllMocks());
 
+    beforeEach(async () => {
+        await resetTestDB()
+    })
+
     afterAll(() => jest.resetAllMocks());
 
+    describe ('get all', () => {
+        it ('resolves with user array', async () => {
+            const all = await User.all;
+            expect(all).toHaveLength(1);
+        })
+    })
+
     describe('create', ()=> {
-        
-        // it('takes in data object and creates a User instance', () => {
-        //     expect.toBeInstanceOf(User); 
-        // });
        
-        it('creates user and generates id', async () => {
+        it('create generates id', async () => {
             
-            jest.spyOn(db, 'query').mockResolvedValueOnce({rows: [ { ...userData, id: 1}] });
+            // jest.spyOn(db, 'query').mockResolvedValueOnce({rows: [ { ...userData, id: 1}] });
             const result = await User.create(userData);
             expect(result).toHaveProperty('id'); 
         });
 
         
-        it('creates user and returns a user instance', async () => {
-        jest.spyOn(db, 'query').mockResolvedValueOnce({rows: [ { ...userData, id: 1}] });
-        let result = await User.create(userData);
-        expect(result).toBeInstanceOf(User);
+        it('create resolves with a User instance', async () => {
+            // jest.spyOn(db, 'query').mockResolvedValueOnce({rows: [ { ...userData, id: 1}] });
+            let result = await User.create(userData);
+            expect(result).toBeInstanceOf(User);
         });
-
-        test.each([
-            [{username: null, password: 'pass'}],
-            [{username: 'test'}],
-            [{password: 'test3'}]
-        ])('rejects empty values in the user test case %#', async (testCase) => {
-            
-            // let result = await User.create(testCase);
-            await expect(User.create(testCase)).rejects.toEqual('ERROR: user could not be created');
-        });
-
-    })
+        describe('create rejects when passed data has wrong values', () => {
+            test.each([
+                [{username: null, password: 'pass'}],
+                [{username: 'test'}],
+                [{password: 'test3'}]
+            ])('empty values %#', async (testCase) => {
+                await expect(User.create(testCase)).rejects.toMatch('ERROR: user could not be created');
+            });
+   
+            it('username already exists', async () => {
+                let existingUsr = {username: 'user_test', password: 'test123'}
+                await expect(User.create(existingUsr)).rejects.toEqual('ERROR: user could not be created\nError: name exists');
+            });
+        });            
+    });
 });
--- db/migrations/1_create_user.sql
+++ db/migrations/1_create_user.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id serial PRIMARY KEY,
-    username VARCHAR(25) NOT NULL,
+    username VARCHAR(25) UNIQUE NOT NULL,
     password varchar(30) NOT NULL 
 );
