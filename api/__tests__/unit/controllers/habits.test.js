f1243c10a097e85e880df8deaa8640888bf2d32b
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-06 18:58:18 +0100

    remove comments

--- api/__tests__/unit/controllers/habits.test.js
+++ api/__tests__/unit/controllers/habits.test.js
@@ -10,25 +10,6 @@ const mockRes = { status: mockStatus }
 
 
 
-//I need to mock the habit.create 
-
-// describe('create', () => {
-//     test('it returns a new book with a 201 status code', async () => {
-//         let testBook = {
-//             id: 2, title: 'Test Book', 
-//             yearOfPublication: 2021,
-//             abstract: 'testing', author_name: 'Bob', author_id: 1
-//         }
-//         jest.spyOn(Book, 'create')
-//             .mockResolvedValue(new Book(testBook));
-            
-//         const mockReq = { body: testBook }
-//         await booksController.create(mockReq, mockRes);
-//         expect(mockStatus).toHaveBeenCalledWith(201);
-//         expect(mockJson).toHaveBeenCalledWith(new Book(testBook));
-//     })
-// });
-
 describe('create', () => {
     test('it returns a new habit with a 201 status code', async () => {
 
