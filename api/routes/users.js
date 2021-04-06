174d320f2d2fa7ac87f40e0c410e6f14eb97fe80
James Wheadon <james.wheadon@yahoo.com>
2021-04-06 17:58:28 +0100

    Fix errors, all tests pass and 97% coverage

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users');
 
-router.post('/register', usersController.create);
 router.post('/login', usersController.find);
 
 module.exports = router;
