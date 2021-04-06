ecb2a020d12daa238837748a45b23448773b76ff
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-06 15:29:57 +0100

    add test for Habit model create

--- api/models/Habit.js
+++ api/models/Habit.js
@@ -1,13 +1,13 @@
-
+const db = require ('../dbConfig/init');
 
 class Habit {
-    contructor(data){
+    constructor(data){
         this.id = data.id;
         this.habit_name = data.habit_name; 
         this.frequency = data.frequency;
-        this.completed = data.completed; //what is this for?
-        this.no_times_completed = data.no_times_completed;  //do we need this?
-        this.last_comp = data.last_comp; // do we need this? can we not just get it from comp_dates table 
+        this.completed = data.completed; 
+        this.no_times_completed = data.no_times_completed; 
+        this.last_comp = data.last_comp; 
         this.comp_dates_id = data.comp_dates_id;
         this.user_id = data.user_id //connects habit to specific user
     }
@@ -15,14 +15,19 @@ class Habit {
 
 //create new habit
 
-    static create(habit_name, frequency, user_id){
+    static async create(habitData){
         return new Promise (async (resolve, reject) => {
             try {
-                let habitData = await db.query(`INSERT INTO habits (habit_name, frequency) VALUES ($1, $2, $3) RETURNING *;`, [ habit_name, frequency, user_id ]);
+            
+               let {habit_name, frequency, user_id} = habitData
+                let result = await db.query(`INSERT INTO habits (habit_name, frequency, user_id) VALUES ($1, $2, $3) RETURNING *;`, [ habit_name, frequency, user_id ]);
                 // I want to get back habit Data with an id, habit_name, frequency, comp_dates_id, the rest need to start at 0 
-                let newHabit = new Habit(habitData.rows[0]);
+                console.log(result.rows[0])
+                let newHabit = new Habit(result.rows[0]);
+                console.log(newHabit)
                 resolve (newHabit);
             } catch (err) {
+                console.log(err)
                 reject('Error creating habit');
             }
         });
