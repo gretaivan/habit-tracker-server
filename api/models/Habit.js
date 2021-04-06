5d56a49644f7201c0441845666a10aa2906b6c44
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-06 13:33:50 +0100

    add user_id to habit model

--- api/models/Habit.js
+++ api/models/Habit.js
@@ -9,16 +9,16 @@ class Habit {
         this.no_times_completed = data.no_times_completed;  //do we need this?
         this.last_comp = data.last_comp; // do we need this? can we not just get it from comp_dates table 
         this.comp_dates_id = data.comp_dates_id;
-        // this.user_id = data.user_id - do I need a user id to connect a specific havit to
+        this.user_id = data.user_id //connects habit to specific user
     }
 
 
 //create new habit
 
-    static create(habit_name, frequency){
+    static create(habit_name, frequency, user_id){
         return new Promise (async (resolve, reject) => {
             try {
-                let habitData = await db.query(`INSERT INTO habits (habit_name, frequency) VALUES ($1, $2) RETURNING *;`, [ habit_name, frequency ]);
+                let habitData = await db.query(`INSERT INTO habits (habit_name, frequency) VALUES ($1, $2, $3) RETURNING *;`, [ habit_name, frequency, user_id ]);
                 // I want to get back habit Data with an id, habit_name, frequency, comp_dates_id, the rest need to start at 0 
                 let newHabit = new Habit(habitData.rows[0]);
                 resolve (newHabit);
